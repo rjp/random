a8c2c40444d9b1d7222e56c364e93ecc5fa0b507
Rob Partington <zimpenfish@gmail.com>
2008-10-13 20:17:52 +0100

    - loop over the last week finding interesting photos

--- interesting_cc.rb
+++ interesting_cc.rb
@@ -7,7 +7,11 @@ allowed = {}
 config = YAML.load_file("#{ENV['HOME']}/.flickr")
 flickr = Flickr.new(api_key = config['api_key'])
 
-x = flickr.interestingness_getList('extras'=>'license', 'date'=>'2008-10-10');
-x['photos']['photo'].find_all {|x| allowed[x['license'].to_i]}.each { |p|
-    p p
+found = []
+today = Time.now
+1.upto(7) { |ago|
+    date = (today - 86400*ago).strftime('%Y-%m-%d')
+    f = flickr.interestingness_getList('extras'=>'license', 'date'=>date);
+    found.push f['photos']['photo'].find_all {|x| allowed[x['license'].to_i]}
 }
+p found.size
