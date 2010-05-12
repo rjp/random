094d5939a3a15cb43b113143dc90f6bbb85354d2
Paul Mison <misonp+git@gmail.com>
2010-05-12 23:51:19 +0800

    Also remove utm_content (and from if there's other utm_ parameters)

--- irssi-utmfb/utmstripper.pl
+++ irssi-utmfb/utmstripper.pl
@@ -7,9 +7,13 @@ sub event_send_text {
     my $newline = $line;
 
     if ($line =~ /http/) { # only bother with URL lines
+        if ($newline =~ /utm_/) {
+            $newline =~ s!from=(.*?)(&|$)!!g;
+        }
         $newline =~ s!utm_source=feedburner&!!g;
         $newline =~ s!utm_medium=feed&!!g;
         $newline =~ s!utm_campaign=(.*?)(&|$)!!g;
+        $newline =~ s!utm_content=(.*?)(&|$)!!g;
         $newline =~ s!\?$!!; # remove a trailing ? if we have no CGI
     }
 
