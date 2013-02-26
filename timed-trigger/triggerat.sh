235ccf9698b8753a7296c485bb28399ca92e62af
rjp <rjpartington@gmail.com>
2013-02-26 16:38:24 +0000

    Also fix up dumb tabs

--- timed-trigger/triggerat.sh
+++ timed-trigger/triggerat.sh
@@ -20,13 +20,13 @@ TM1=$((IV-1))
 
 while [ 1 ]; do
     # Sleep until TM2, busy wait until TM1
-	while [ $(((SECONDS-offset) % IV)) -lt $TM2 ]; do sleep 1; done
-	while [ $(((SECONDS-offset) % IV)) -lt $TM1 ]; do echo >/dev/null; done
+    while [ $(((SECONDS-offset) % IV)) -lt $TM2 ]; do sleep 1; done
+    while [ $(((SECONDS-offset) % IV)) -lt $TM1 ]; do echo >/dev/null; done
 
     # This might get pre-empted and take longer than 1s
     # The alternative is more busy-waiting at the expense of CPU
-	sleep 1
+    sleep 1
 
     # Run the command, quit if it fails
-	$COM || exit
+    $COM || exit
 done
