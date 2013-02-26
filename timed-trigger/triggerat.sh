fcfd535000b5f3e787188ad99b506d9e2569a35c
rjp <rjpartington@gmail.com>
2013-02-26 16:35:56 +0000

    Documentation

--- timed-trigger/triggerat.sh
+++ timed-trigger/triggerat.sh
@@ -1,17 +1,32 @@
+#! bash
+
+# This is probably better in an ENV than an argument
 IV=${1:-20}
 shift
+
+# Almost certainly this will cause quoting nightmares
 COM=${@:-date}
+
+# Work out the offset between real time and 'local' time
 boot=$((SECONDS % IV))
 time=$(($(date +%s) % IV))
 offset=$((IV-(time-boot)))
+
 echo "boot=$boot time=$time offset=$offset"
 
-IV_T1=$((IV-2))
-IV_T2=$((IV-1))
+# Constant-fold these out of the loop
+TM2=$((IV-2))
+TM1=$((IV-1))
 
 while [ 1 ]; do
-	while [ $(((SECONDS-offset) % IV)) -lt $IV_T1 ]; do sleep 1; done
-	while [ $(((SECONDS-offset) % IV)) -lt $IV_T2 ]; do echo >/dev/null; done
+    # Sleep until TM2, busy wait until TM1
+	while [ $(((SECONDS-offset) % IV)) -lt $TM2 ]; do sleep 1; done
+	while [ $(((SECONDS-offset) % IV)) -lt $TM1 ]; do echo >/dev/null; done
+
+    # This might get pre-empted and take longer than 1s
+    # The alternative is more busy-waiting at the expense of CPU
 	sleep 1
+
+    # Run the command, quit if it fails
 	$COM || exit
 done
